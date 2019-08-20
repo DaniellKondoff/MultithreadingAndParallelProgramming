f5a1489b855021bdfddf624e24bb76edf535dedd
DKondoff <danielkondov91gmail.com>
2019-08-20 16:55:53 +0300

    SemaphoreSlim

--- Synchronization/Startup.cs
+++ Synchronization/Startup.cs
@@ -8,14 +8,18 @@ namespace Synchronization
     class Startup
     {
         public static SemaphoreSlim Bouncer { get; set; }
+        private static SemaphoreSlim semaphore;
+        private static int padding;
 
         static void Main(string[] args)
         {
-            Bouncer = new SemaphoreSlim(3, 3);
+            //Bouncer = new SemaphoreSlim(3, 3);
 
-            OpenNightClub();
+            //OpenNightClub();
 
-            Thread.Sleep(20000);
+            //Thread.Sleep(20000);
+
+            SemaphoreSlimExample();
             Console.Read();
         }
 
@@ -37,7 +41,7 @@ namespace Synchronization
             Thread.Sleep(500);
 
             Console.WriteLine($"Guest {guestNumber} is leaving the nightclub.");
-            Bouncer.Release(1);
+            Bouncer.Release(3);
 
             Console.WriteLine();
         }
@@ -73,6 +77,45 @@ namespace Synchronization
             Task.WaitAll(tasks.ToArray());
 
             Console.WriteLine($"Result is: {c.Armor}");
+        }
+
+        public static void SemaphoreSlimExample()
+        {
+            semaphore = new SemaphoreSlim(0, 3);
+            Console.WriteLine("{0} tasks can enter the semaphore.", semaphore.CurrentCount);
+
+            Task[] tasks = new Task[5];
+
+            // Create and start five numbered tasks.
+            for (int i = 0; i <= 4; i++)
+            {
+                tasks[i] = Task.Run(() => {
+                    // Each task begins by requesting the semaphore.
+                    Console.WriteLine("Task {0} begins and waits for the semaphore.", Task.CurrentId);
+                    semaphore.Wait();
+
+                    Interlocked.Add(ref padding, 100);
+
+                    Console.WriteLine("Task {0} enters the semaphore.", Task.CurrentId);
+
+                    // The task just sleeps for 1+ seconds.
+                    Thread.Sleep(1000 + padding);
+
+                    Console.WriteLine("Task {0} releases the semaphore; previous count: {1}.", Task.CurrentId, semaphore.Release());
+                });
+            }
+
+            // Wait for half a second, to allow all the tasks to start and block.
+            Thread.Sleep(500);
+
+            // Restore the semaphore count to its maximum value.
+            Console.Write("Main thread calls Release(3) --> ");
+            semaphore.Release(3);
+            Console.WriteLine("{0} tasks can enter the semaphore.", semaphore.CurrentCount);
+            // Main thread waits for the tasks to complete.
+            Task.WaitAll(tasks);
+
+            Console.WriteLine("Main thread exits.");
         }
     }
 }
